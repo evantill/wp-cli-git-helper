84763cc146dfd0966ef50b9f3dd430e891f36642
Eric Vantillard <eric.vantillard@evaxion.fr>
2020-06-03 18:25:07 +0200

    Fix autoload.

--- wp-cli-git-helper.php
+++ wp-cli-git-helper.php
@@ -4,4 +4,9 @@ if ( ! class_exists( 'WP_CLI' ) ) {
 	return;
 }
 
+$autoload = dirname( __FILE__ ) . '/vendor/autoload.php';
+if ( file_exists( $autoload ) ) {
+	require_once $autoload;
+}
+
 WP_CLI::add_command( 'gh', '\boonebgorges\WPCLIGitHelper\Command' );
